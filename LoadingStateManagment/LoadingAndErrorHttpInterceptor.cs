55bbf45a008e6000111d7200842baf6339955bcd
Geraldson Perez <31606909+Geraldsonp@users.noreply.github.com>
2024-10-20 14:30:00 -0400

    Merge pull request #12
    * completes

---
+++ LoadingStateManagment/LoadingAndErrorHttpInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using PersonalFinanceApp.ErrorHandling;
+
+namespace PersonalFinanceApp.LoadingStateManagment;
+
+public class LoadingAndErrorHttpInterceptor: DelegatingHandler
+{
+    private readonly LoadingService _loadingService;
+    private readonly IErrorHandler _errorHandler;
+
+    public LoadingAndErrorHttpInterceptor(LoadingService loadingService, IErrorHandler errorHandler)
+    {
+        _loadingService = loadingService;
+        _errorHandler = errorHandler;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        _loadingService.IsLoading = true;
+        try
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+            if (!response.IsSuccessStatusCode)
+            {
+                _errorHandler.HandleError(new HttpRequestException($"HTTP request failed with status code {response.StatusCode}"));
+            }
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _errorHandler.HandleError(ex);
+            throw;
+        }
+        finally
+        {
+            _loadingService.IsLoading = false;
+        }
+    }
+}
